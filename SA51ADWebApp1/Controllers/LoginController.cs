7bda5b86362e2e148d28c747588e3e36c24f2d7f
DeJustin <makebelief@icloud.com>
2021-02-01 18:28:02 +0800

    Testing Login view

--- SA51ADWebApp1/Controllers/LoginController.cs
+++ SA51ADWebApp1/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SA51ADWebApp1.Models;
+using SA51ADWebApp1.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +10,25 @@ namespace SA51ADWebApp1.Controllers
 {
     public class LoginController : Controller
     {
+        protected Database dbcontext;
+
+        public LoginController (Database dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
         [Route("/")]
         [Route("/login")]
         public IActionResult DisplayLoginForm()
         {
-            return View("Login");
+            Admin newLogin = new Admin();
+            return View(newLogin);
+        }
+
+        [HttpPost]
+        public IActionResult Login(Admin newLogin)
+        {
+            return View("Home");
         }
 
         [Route("/logout")]
