6b56e429ea6e7eb8ecb134f3799973c6a331406b
catintheclouds <makebelief@icloud.com>
2021-02-09 14:47:36 +0800

    Fixed transactions.

---
+++ SA51ADWebApp1/Service/ITransactionService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using SA51ADWebApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SA51ADWebApp1.Service
+{
+    public interface ITransactionService
+    {
+        public void saveTransaction(StationOnLine sol, int userId);
+    }
+}
